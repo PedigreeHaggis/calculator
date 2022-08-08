f10dc9bb0790aa1ee96048338758e07cbbd3efdd
Gary Taylor <garycharlestaylor@outlook.com>
2022-08-08 19:31:39 +0100

    html css first version

---
+++ resources/index.js
@@ -0,0 +1,20 @@
+function add(n1,n2){
+    return n1+n2;
+}
+
+function subtract(n1,n2){
+    return n1 - n2;
+}
+
+function multiply(n1,n2){
+    return n1 * n2
+}
+
+function divide(n1,n2){
+    return n1 / n2;
+}
+
+function operate(n1, n2, operator){
+    operator(n1,n2);
+}
+
